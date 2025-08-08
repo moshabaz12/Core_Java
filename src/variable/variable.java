67e173829c405715542734ff5294672165c0668c
moshabaz12 <shabazkhan68224@gmail.com>
2025-08-08 20:33:52 +0530

    follow me

---
+++ src/variable/variable.java
@@ -0,0 +1,26 @@
+package variable;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+int [][][]a= {{{1,2,3},{4,5,6}},{{6,8,9,10},{11,12,13}}};
+ System.out.println(a[1][0][2]);
+	
+ System.out.println(a .length);
+ 
+
+ 
+ String ne="shabaz";
+ System.out.println(ne.length());
+ 
+ int [][] df=new int [3][2];
+ System.out.println(df[0].length);
+ 
+ int [] y= {9,8,7,6};
+ System.out.println(y.length);
+ System.out.println(y[0]);
+	}}
+
+
